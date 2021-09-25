541fca6a2c919105107baf23ad7016df893327e5
Alefew Yimer <alefewdev21@gmail.com>
2021-09-25 23:20:30 +0300

    fix:add height

--- lib/widgets/reusable/custom_card.dart
+++ lib/widgets/reusable/custom_card.dart
@@ -3,28 +3,35 @@ import 'package:flutter/material.dart';
 class CustomCard extends StatelessWidget {
   final Widget child;
   final double width;
+  final double height;
   const CustomCard({
     Key? key,
     required this.child,
     required this.width,
+    required this.height,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      child: Card(
-        elevation: 0.6,
-        borderOnForeground: true,
-        clipBehavior: Clip.hardEdge,
-        shadowColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-          side: BorderSide(
-            color: Colors.grey.shade200,
-            width: 0.6,
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: width,
+        height: height,
+        child: Card(
+          semanticContainer: true,
+          elevation: 0.6,
+          // borderOnForeground: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          shadowColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(
+              color: Colors.grey.shade200,
+              width: 0.6,
+            ),
           ),
+          child: child,
         ),
-        child: child,
       ),
     );
   }
